2e8d982de407eedc1d70767d664e5611ceaeba19
pecko057 <andrej.pecanac.atia@gmail.com>
2018-05-31 00:30:03 +0200

    Update Ocjene.java

--- Ocjene/src/ocjene/Ocjene.java
+++ Ocjene/src/ocjene/Ocjene.java
@@ -5,6 +5,7 @@
  */
 package ocjene;
 import java.util.Scanner;
+import jdk.internal.org.objectweb.asm.commons.Method;
 /**
  *
  * @author Andrej
@@ -18,53 +19,59 @@ public class Ocjene {
         
         String[] predmeti = {"Matematika", "Fizika", "Biologija", "Hemija", "Geografija"};
         int [] ocjene = {0, 0, 0, 0, 0};
+        
+        
+        
         String [][] testovi = {
-            {"matematikaTest1", "fizikaTest1", "biologijaTest1", "hemijaTest1", "geografijaTest1"},
+            {"kviz1", "fizikaTest1", "biologijaTest1", "hemijaTest1", "geografijaTest1"},
             {"matematikaTest2", "fizikaTest2", "biologijaTest2", "hemijaTest2", "geografijaTest2"},
             {"matematikaTest3", "fizikaTest3", "biologijaTest3", "hemijaTest3", "geografijaTest3"},
         };
         
         Scanner unos = new Scanner(System.in);
         
-        //Unosi se int broj bodova
+//  Petlja za prolaz kroz sve ispite
         for (int i = 0; i < 1; i++) {
         
+//  Petlja za prolaz kroz tri kviza
         System.out.println(predmeti[i]);
             for (int j = 0; j < 3; j++) {
-                System.out.println("Da li je radjen kviz " + (j+1) + "? (da/ne)");
-                String k1 = unos.next();
                 
+//  Provjera unesenog odgovora da ili ne.
+//  Petlja WHILE vraca na unos ako je odgovor razlicit od "da" ili "ne"
+//  SWITCH usmjerava na unos bodova odnosno kviz za odgovore "da" odnosno "ne"
+                String k1 = null;
+                while (!("da".equals(k1) || "ne".equals(k1))) {                    
+                    System.out.println("Da li je radjen kviz " + (j+1) + "? (da/ne)");
+                    k1 = unos.next();
+                }
                 switch (k1){
                     case "da":                
                         ocjene[i] += bodovi10();
                         break;
                     case "ne":
-                        System.out.println("Odgovorite na sljedeca pitanja:");
-                        System.out.println(testovi[j][i]);
-                        // pitanja();                          //pozivanje metode pitanja() da bi se pristupilo kvizu i bodovanju odgovora
-                        //System.out.println(pitanja());      //iz nekog razloga dva puta postavlja pitanje 1
-                        ocjene[i] += pitanja();
-
+                        System.out.println("Odgovorite na sljedeca pitanja:\n");
+                        ocjene[i] += kviz1();
                         break;
                 }
             }   
-//          Unos bodova sa pismenog dijela ispita
-//          Bodovi se unose kao integer i ispituje se tacnost unosa metodom bodovi20
+//  Unos bodova sa pismenog dijela ispita
+//  Bodovi se unose kao integer i ispituje se tacnost unosa metodom bodovi20
             System.out.println("Unesite bodove sa pismenog dijela ispita:");
                 ocjene[i] += bodovi20();
                 
-//          Unos bodova sa usmenog dijela ispita
-//          Bodovi se unose kao integer i ispituje se tacnost unosa metodom bodovi20
+//  Unos bodova sa usmenog dijela ispita
+//  Bodovi se unose kao integer i ispituje se tacnost unosa metodom bodovi20
             System.out.println("Unesite bodove sa usmenog dijela ispita:");
                 ocjene[i] += bodovi20();
                 
-//          Unos bodova sa seminarskog rada
-//          Bodovi se unose kao integer i ispituje se tacnost unosa metodom bodovi20
+//  Unos bodova sa seminarskog rada
+//  Bodovi se unose kao integer i ispituje se tacnost unosa metodom bodovi20
             System.out.println("Unesite bodove seminarskog rada:");
                 ocjene[i] += bodovi20();
         
-//          Unos bodova sa prisustva
-//          Bodovi se unose kao integer i ispituje se tacnost unosa metodom bodovi10
+//  Unos bodova sa prisustva
+//  Bodovi se unose kao integer i ispituje se tacnost unosa metodom bodovi10
             System.out.println("Unesite bodove prisustva:");
                     ocjene[i] += bodovi10();
 
@@ -72,69 +79,79 @@ public class Ocjene {
                 
                 
         
-//      Ispis rezultata po predmetima
+//  Ispis rezultata po predmetima, izracunavanje i ispis prosjeka
+        int prosjek=0;
+        System.out.println("\nStudent je ostvario sljedeci uspjeh:\n");
             for (int i = 0; i < predmeti.length; i++) {
-                System.out.println(predmeti[i] + ": " + ocjene[i]);
+                System.out.println(predmeti[i] + ": " + ocjene[i]+"%");
+                prosjek += ocjene[i];
         }
+            prosjek /= predmeti.length;
+        System.out.println("\nPROSJECNO:" + prosjek + "%");
             
             
-        }
+}
+    
+    public static int kviz1(){
+        Scanner odg = new Scanner (System.in);
+        String [] pitanja = { "Mlijeko je bijelo? \n a) da\n b) ne",
+                                "5 > 10\n a) da\n b) ne",
+                                "Svjetlost je brza od zvuka. \n a) da\n b) ne",
+                                "Miki Maus je mis. \n a) da\n b) ne",
+                                "H2O je hemijska formula benzina. \n a) da\n b) ne"};
+        String [] odgovori = {"a", "b", "a", "a", "b"};
+        int bod=0;
+            for (int i = 0; i < pitanja.length; i++) {
+                System.out.println(pitanja[i]);
+                String odgovor = null;
+                    while (!("a".equals(odgovor) || "b".equals(odgovor))) {                    
+                        System.out.println("ODGOVOR:");
+                        odgovor = odg.next();
+                    }
+                        if (odgovor.equals(odgovori[i])) {
+                        bod += 2;
+                        }
+            }
+        return bod;
+    }
     
     public static int bodovi10(){
-                Scanner unos = new Scanner (System.in);
-                int kviz1=15;
-                while (kviz1<0 || kviz1>10) {            
-                    try {
-                            System.out.print("Unesite broj bodova: ");
-                            kviz1 = unos.nextInt();
-                            if (kviz1 < 0 || kviz1 > 10) throw new IllegalArgumentException();
-                        }
-                        catch (IllegalArgumentException iae) {
-                            System.out.println("Broj bodova nije u opsegu 0-10!");
-                        }
-                        catch (Exception e) {
-                            System.out.println("Niste pravilno unijeli broj bodova!");
-                            unos.next();
-                        }
+        Scanner unos = new Scanner (System.in);
+        int kviz1=15;
+        while (kviz1<0 || kviz1>10) {            
+            try {
+                    System.out.print("Unesite broj bodova: ");
+                    kviz1 = unos.nextInt();
+                    if (kviz1 < 0 || kviz1 > 10) throw new IllegalArgumentException();
                 }
-                return kviz1;
+                catch (IllegalArgumentException iae) {
+                    System.out.println("Broj bodova nije u opsegu 0-10!");
+                }
+                catch (Exception e) {
+                    System.out.println("Niste pravilno unijeli broj bodova!");
+                    unos.next();
+                }
+        }
+        return kviz1;
     }
     
-        public static int bodovi20(){
-                Scanner unos = new Scanner (System.in);
-                int kviz1=25;
-                while (kviz1<0 || kviz1>20) {            
-                    try {
-                            System.out.print("Unesite broj bodova: ");
-                            kviz1 = unos.nextInt();
-                            if (kviz1 < 0 || kviz1 > 20) throw new IllegalArgumentException();
-                        }
-                        catch (IllegalArgumentException iae) {
-                            System.out.println("Broj bodova nije u opsegu 0-20!");
-                        }
-                        catch (Exception e) {
-                            System.out.println("Niste pravilno unijeli broj bodova!");
-                            unos.next();
-                        }
+    public static int bodovi20(){
+        Scanner unos = new Scanner (System.in);
+        int kviz1=25;
+        while (kviz1<0 || kviz1>20) {            
+            try {
+                    System.out.print("Unesite broj bodova: ");
+                    kviz1 = unos.nextInt();
+                    if (kviz1 < 0 || kviz1 > 20) throw new IllegalArgumentException();
                 }
-                return kviz1;
-    }
-    public static int pitanja(){                // započeta metoda pitanja()
-       int [] pitanje={1, 2, 3, 4, 5, 6, 7, 8, 9};
-       int odg, bod=0, i;
-        for (i = 0; i < 2; i++) {
-             System.out.println((i+1) + ". Pitanje "+(i+1)+"?");
-                System.out.print((i+1) + ". Odgovor "+(i+1)+": ");
-        Scanner scan=new Scanner (System.in);
-            odg=scan.nextInt();    
-        if (odg==pitanje[i]){
-            bod=bod+5;
-        } else bod=bod+0;
-            bod=+bod;
-            System.out.println("bodovi "+bod);
+                catch (IllegalArgumentException iae) {
+                    System.out.println("Broj bodova nije u opsegu 0-20!");
+                }
+                catch (Exception e) {
+                    System.out.println("Niste pravilno unijeli broj bodova!");
+                    unos.next();
+                }
         }
-            return bod;
+        return kviz1;
     }
-
-
 }
